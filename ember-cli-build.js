028fe900f45ef2247e5889e7cc57c5ef6e48d036
Andy Brown <andy.brown@thetrainline.com>
2021-03-10 12:42:44 +0000

    WIP - include ravelin library

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('vendor/ravelinjs-1.1.2-core+track+encrypt+promise.min.js');
   return app.toTree();
 };
